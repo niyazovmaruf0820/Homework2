65ea360e1ef6c7f45197e59c1d3abfa8448d23e6
Maruf Niyazov <niyazovmaruf0820@gmail.com>
2023-12-20 14:14:58 +0300

    first commit

---
+++ task1/Program.cs
@@ -0,0 +1,34 @@
+﻿System.Console.WriteLine("Enter your FirstName : ");
+string firstname = Console.ReadLine();
+System.Console.WriteLine("Enter your LastName : ");
+string lastname = Console.ReadLine();
+System.Console.WriteLine("Enter your Age : ");
+int age = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Enter your Address : ");
+string address = Console.ReadLine();
+System.Console.WriteLine("Enter your FulName : ");
+string fulname = Console.ReadLine();
+System.Console.WriteLine("Enter your birth Year  : ");
+int birthyear = Convert.ToInt32(Console.ReadLine());
+Person person = new Person();
+person.FirstName = firstname;
+person.LastName = lastname;
+person.Age = age;
+person.Address = address;
+person.FullName = fulname;
+person.BirthYear = birthyear;
+System.Console.WriteLine(person.Information());
+
+class Person
+{
+    public string FirstName;
+    public string LastName;
+    public int Age;
+    public string Address;
+    public string FullName;
+    public int BirthYear;
+    public string Information()
+    {
+        return $"My name is {FullName} \nMy birth year is {BirthYear}.";
+    }
+}
---
+++ task2/Program.cs
@@ -0,0 +1,16 @@
+﻿int w = Convert.ToInt32(Console.ReadLine());
+int h = Convert.ToInt32(Console.ReadLine());
+Rectangle rectangle = new Rectangle();
+rectangle.width = w;
+rectangle.height = h;
+System.Console.WriteLine(rectangle.Area());
+class Rectangle {
+    int ar = 0;
+        public int width;
+        public int height;
+        public int Area()
+        {
+            ar = width * height;
+            return  ar;
+        }
+    }
